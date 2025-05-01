d6c21eb7cebcaca283a22dffbc4ef863993aff60
AlexNgooi <alexngooixueyang@gmail.com>
2025-05-01 15:22:47 +0800

    commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ticket_app/base/bottom_nav_bar.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/utils/app_routes.dart';
+import 'package:ticket_app/screens/home/all_hotels.dart';
+import 'package:ticket_app/screens/home/all_tickets.dart';
+import 'package:ticket_app/screens/hotel_detail.dart';
+import 'package:ticket_app/screens/ticket/ticket_screen.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+      ProviderScope(child: const MyApp())
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return   MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        AppRoutes.homePage:(context)=>  BottomNavBar(),
+        AppRoutes.allTickets:(context)=> const AllTickets(),
+        AppRoutes.ticketScreen:(context)=> const TicketScreen(),
+        AppRoutes.allHotels:(context)=> const AllHotels(),
+        AppRoutes.hotelDetail:(context)=> const HotelDetail()
+      },
+    );
+  }
+}
